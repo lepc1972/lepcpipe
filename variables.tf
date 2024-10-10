db974076fbe35efc946105054fcb84721d6444b4
luis eduardo <githublepc@gmail.com>
2024-10-10 18:27:40 -0500

    Update variables.tf bucket name

--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@ variable "key_name" {
 
 variable "s3_bucket_name" {
   description = "Name of the S3 bucket for remote state"
-  default = "codepipeline-us-east-2-333682311814"
+  default = "bucketlepc"
 }
 
 variable "name" {
